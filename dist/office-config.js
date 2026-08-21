9c94ef1b313e8eeb724918e00438549f707efdf5
nvenu <nvenu@solifetec.com>
2026-08-21 12:55:07 +0530

    fix: update office dropdown values for new Kinnser login
    Office values are user-specific in Kinnser. Updated to match the
    new login credentials (Vipin Sharma) dropdown values.

--- dist/office-config.js
+++ dist/office-config.js
@@ -2,16 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.OFFICES = void 0;
 exports.OFFICES = [
-    { value: "1407132,Clinic", name: "Nightingale - Taunton", stateCode: "MA" },
-    { value: "1407125,Clinic", name: "Aspire - Dublin", stateCode: "OH" },
-    { value: "1407126,Clinic", name: "Aspire - San Diego", stateCode: "SD" },
-    { value: "1407127,Clinic", name: "Aspire - Scottsdale", stateCode: "AZ" },
-    { value: "1407128,Clinic", name: "Aspire - Yuba City", stateCode: "YC" },
-    { value: "1407129,Clinic", name: "Nightingale - Las Vegas", stateCode: "NV" },
-    { value: "1407130,Clinic", name: "Nightingale - Minnetonka", stateCode: "MN" },
-    { value: "1407131,Clinic", name: "Nightingale - Pompano Beach", stateCode: "FL" },
-    { value: "1417923,Clinic", name: "Nightingale - Stamford", stateCode: "CT" },
-    { value: "1407133,Clinic", name: "Nightingale - Willowbrook", stateCode: "IL" }
+    { value: "1406963,Clinic", name: "Nightingale - Taunton", stateCode: "MA" },
+    { value: "1406956,Clinic", name: "Aspire - Dublin", stateCode: "OH" },
+    { value: "1406957,Clinic", name: "Aspire - San Diego", stateCode: "SD" },
+    { value: "1406958,Clinic", name: "Aspire - Scottsdale", stateCode: "AZ" },
+    { value: "1406959,Clinic", name: "Aspire - Yuba City", stateCode: "YC" },
+    { value: "1406960,Clinic", name: "Nightingale - Las Vegas", stateCode: "NV" },
+    { value: "1406961,Clinic", name: "Nightingale - Minnetonka", stateCode: "MN" },
+    { value: "1406962,Clinic", name: "Nightingale - Pompano Beach", stateCode: "FL" },
+    { value: "1426604,Clinic", name: "Nightingale - Stamford", stateCode: "CT" },
+    { value: "1406964,Clinic", name: "Nightingale - Willowbrook", stateCode: "IL" }
 ];
 // Skip Corporation level
 // { value: "75945,Corporation", name: "Homecare Providers", stateCode: "ALL" }
